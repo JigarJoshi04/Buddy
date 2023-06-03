96383a0aebfc8de1be465c7a4ec4e76461589b46
Jigar Joshi <jigar.pj@somaiya.edu>
2023-06-03 16:59:03 +0530

    openAI API key added

--- openAI.py
+++ openAI.py
@@ -1,9 +1,11 @@
+import os
 import openai
 
 
 class OpenAI:
     def __init__(self):
         self.model = "gpt-3.5-turbo"
+        openai.api_key = os.getenv('OPEN_API_KEY')
 
     def chat_completion_api(self, messages):
         response = openai.ChatCompletion.create(
